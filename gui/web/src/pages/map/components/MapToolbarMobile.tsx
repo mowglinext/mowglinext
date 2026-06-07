1190b5038133387f0bbd1e58d541bec0fe388da2
Yann Lossouarn <yann@lossouarn.net>
2026-06-07 17:28:04 +0200

    fix(gui): increase mobile toolbar bottom offset to 108px

--- gui/web/src/pages/map/components/MapToolbarMobile.tsx
+++ gui/web/src/pages/map/components/MapToolbarMobile.tsx
@@ -114,7 +114,7 @@ export const MapToolbarMobile = ({
 
     const toolbarStyle: React.CSSProperties = {
         position: "absolute",
-        bottom: 96,
+        bottom: 108,
         left: 12,
         right: 12,
         zIndex: 10,
